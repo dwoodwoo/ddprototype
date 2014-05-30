13ab31708830326bf2ec75aa898e513f15035f01
Dennis Woo <dwoo@sent.com>
2014-05-30 12:24:56 -0700

    added: debounce directive

--- js/controllers/searchController.js
+++ js/controllers/searchController.js
@@ -1,6 +1,6 @@
 // var filterApp = angular.module('filterApp', ['ui.bootstrap', 'angles', 'ngAnimate', 'duScroll', 'firebase'])
 
-app.controller('searchController', function($scope, $document, searchService) {
+app.controller('searchController', function($scope, $document, searchService, $timeout) {
 
         // DONE:  Link from filtering search form to specific candidate result page
         $scope.init = function(){
@@ -10,9 +10,26 @@ app.controller('searchController', function($scope, $document, searchService) {
         // runs once per controller instantiation
         $scope.init();
 
-        $scope.onKeyPress = function($event) {
-            console.log($event.charCode);
-            console.log($event.altKey);
-            console.log($event.ctrlKey);
-            };
+        // $scope.onKeyPress = function($event) {
+        //     console.log($event.charCode);
+        //     console.log($event.altKey);
+        //     console.log($event.ctrlKey);
+        //     };
+
+
+
+//debounce http://stackoverflow.com/questions/15304562/how-to-put-a-delay-on-angularjs-instant-search
+        $scope.filterText = '';
+
+        // Instantiate these variables outside the watch
+        var tempFilterText = '',
+            filterTextTimeout;
+        $scope.$watch('search', function (val) {
+            if (filterTextTimeout) $timeout.cancel(filterTextTimeout);
+
+            tempFilterText = val;
+            filterTextTimeout = $timeout(function() {
+                $scope.filterText = tempFilterText;
+            }, 2000); // delay 250 ms
+        })
 });
